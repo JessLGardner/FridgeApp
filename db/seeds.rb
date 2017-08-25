cda0b27f711a09090ea281e9afca397d47edd1e6
Jess Gardner <inanna1130@gmail.com>
2017-08-25 00:34:26 -0400

    None of this works and its almost 1 am

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 Fridge.destroy_all
 
 rihanna = Fridge.create(location: "kitchen", brand: "Ikea", size: 20, has_food: true, has_drink: true)
+drake = Fridge.create(location: "dorm", brand: "Sears", size: 10, has_food: false, has_drink: true)
 
 rihanna.food = [
     Food.create(fridge_id: rihanna.id, name: "hot dog", weight: 10, is_vegan: false),
@@ -12,4 +13,8 @@ rihanna.drink = [
     Drink.create(fridge_id: rihanna.id, name: "pop", size: 12, is_alcoholic: false),
     Drink.create(fridge_id: rihanna.id, name: "beer", size: 40, is_alcoholic: true),
     Drink.create(fridge_id: rihanna.id, name: "almond milk", size: 8, is_alcoholic: false)
+]
+
+drake.food = [
+    Food.create(fridge_id: drake.id, name: "cheese", weight: 10, is_vegan: false)
 ]
--- lib/fridge_tracker.rb
+++ lib/fridge_tracker.rb
@@ -8,33 +8,37 @@ puts "B) Create a new Fridge"
 puts "C) Remove a Fridge"
 
 option = gets.chomp
-puts "You selected #{option}"
+puts "You selected #{option.upcase}"
 
 if option.upcase == 'A'
   all_fridges = Fridge.all
   all_fridges.each_with_index do |fridge|
     puts fridge.location
   end
+    fridge_selected = gets.chomp
+    Fridge.find_by_location(fridge_selected).Food.all
+    # puts fridge_selected.Food.all
+
+# Food.joins(:fridge).where('Fridge.location = fridge_selected')
+
+
+
 
 elsif option.upcase == 'B' 
-  puts "enter fridge location"
+  puts "Enter fridge location."
   location = gets.chomp
-
-  puts "enter fridge brand"
+  puts "Enter fridge brand."
   brand = gets.chomp
-
-  puts "enter fridge size in cubic feet"
+  puts "Enter fridge size in cubic feet."
   size = gets.chomp
-
-  puts "does your fridge have food? (y/n)"
+  puts "Does your fridge have food? (y/n)"
   has_food = gets.chomp
     if has_food == "y"
       has_food = true
     else
       has_food = false
     end
-
-  puts "does your fridge have drinks? (y/n)"
+  puts "Does your fridge have drinks? (y/n)"
   has_drink = gets.chomp
     if has_drink == "y"
       has_drink = true
@@ -45,15 +49,26 @@ elsif option.upcase == 'B'
   Fridge.create(location: location, brand: brand, size: size, has_food: has_food, has_drink: has_drink)
 
 elsif option.upcase == 'C'
-  puts "Which fridge do you want to remove? (enter Location)"
+  puts "Which fridge do you want to remove? (enter location)"
   fridge_to_remove = gets.chomp
   puts "Removing #{fridge_to_remove}"
   Fridge.find_by_location(fridge_to_remove).destroy
-
+else
+  puts "Oops! You must select 'A', 'B' or 'C.'"
 end
 
 
 
 
+
+
+# TO DO
+# View all food items in a specific fridge
+# Add a food item to a fridge
+# Eat a food item from a fridge (delete it)
+# View all drink items in a specific fridge
+# Add a drink item to a fridge
+# Consume PART of a drink from a fridge (update its size in ounces)
+# Consume ALL of a drink from a fridge (delete it)
 
 
